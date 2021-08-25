42044ee6d2a3b3cf0e057232de9e9eb6d15f7479
svet-b <svet.bajlekov@ammp.io>
2021-08-25 16:52:36 +0200

    Fix up metadata

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Python',
-    version='1.0.0',
+    name='openwrt-test',
+    version='0.1',
     url='https://github.com/svet-b/python-openwrt-test.git',
     author='Svet Bajlekov',
     author_email='tomatoman@gmail.com',
-    description='Test',
+    description='OpenWRT Test for Python package',
+    python_requires='~=3.7',
     packages=find_packages(),    
     install_requires=['paho-mqtt'],
     py_modules=[
         'mqtt_sub',
     ],
-    entry_points={
-        'console_scripts': [
-            'mqtt_sub = mqtt_sub:main',
-        ]
-    },
 )
